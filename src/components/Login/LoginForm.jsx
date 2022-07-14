e48e4a00736577fb0767620991d5ad5466a37df3
Fenna <ikbenfenna@gmail.com>
2022-07-14 13:28:43 +0200

    Can now create and check users

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,20 +1,42 @@
 import {useForm} from 'react-hook-form'
 import React from 'react'
-function SimpleForm() {
+const usernameConfig = {
+    required: true,
+    minLength: 2
+}
+function LoginForm() {
+    // Destructuring of useForm() hook so we can use register, handleSubmit and formState later on 
     const { register, handleSubmit, formState: { errors }} = useForm()
 
+    /*  Function that sends data
+        INPUT: Data object
+        OUTPUT: Sends data to API to retreive login info
+    */
     const handleonSubmit = (data) => {
         // Send data to API
-        console.log(data.username)
+        
     }
+    /*  Function to deal with errors
+        INPUT: Any errors that came up
+        OUTPUT: <p> element that contains the errormessage
+    */
+    const errorMessage = (() => {
+        if (!errors.username)
+            return null
+        if (errors.username.type === 'required')
+            return <p>Username is required!</p>
+        if (errors.username.type === 'minLength')
+            return <p>Username must be of at least 2 characters</p>
+    })()
     return (
         <>
+        <h1>What is your name?</h1>
             <form onSubmit={handleSubmit(handleonSubmit)}>
-                <input type="text" placeholder='Username' {...register("username",{required: true, minLength: 2})} />
+                <input type="text" placeholder='Username' {...register("username",usernameConfig)} />
                 <button type="submit">Submit</button>
-                { errors.username && <p>Username must be of at least 2 characters!</p>}
+                { errorMessage }
             </form>
         </>
     )
 }
-export default SimpleForm
+export default LoginForm
