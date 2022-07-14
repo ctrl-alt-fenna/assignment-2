b401c3fc19193d59a370598b6866a479557e495d
Fenna <ikbenfenna@gmail.com>
2022-07-14 12:31:25 +0200

    Added simple form, removed logos

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,1 +1,20 @@
+import {useForm} from 'react-hook-form'
+import React from 'react'
+function SimpleForm() {
+    const { register, handleSubmit, formState: { errors }} = useForm()
 
+    const handleonSubmit = (data) => {
+        // Send data to API
+        console.log(data.username)
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit(handleonSubmit)}>
+                <input type="text" placeholder='Username' {...register("username",{required: true, minLength: 2})} />
+                <button type="submit">Submit</button>
+                { errors.username && <p>Username must be of at least 2 characters!</p>}
+            </form>
+        </>
+    )
+}
+export default SimpleForm
