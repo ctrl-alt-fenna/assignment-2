ff66b04131fb472d791e479494c20e25ad42078e
Fenna <ikbenfenna@gmail.com>
2022-07-16 20:34:47 +0200

    MOD: Profile now shows only first 10 translations

--- src/components/Profiles/ProfileTranslateHistory.jsx
+++ src/components/Profiles/ProfileTranslateHistory.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 const ProfileTranslateHistory = ({ translations }) => {
 
   const translatedHistory = translations.map(
-    (translationItem, index) => <ProfileTranslationHistoryItem key={index + '_' + translationItem} translationItem={translationItem} />)
+    (translationItem, index) => <ProfileTranslationHistoryItem key={index + '_' + translationItem} translationItem={translationItem} count={index}/>)
 
   return (
     <section>
       <h4>Translation History</h4>
-      <ul>
         {translatedHistory}
-      </ul>
     </section>
   )
 }
