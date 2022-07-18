cf140ea8a1bc48275d82a591ec284d585add5bce
fransijn <ikbenfenna@gmail.com>
2022-07-18 14:05:48 +0200

    Removed unused function

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,26 +1,15 @@
 import ProfileAction from '../components/Profiles/ProfileAction';
 import ProfileHeader from "../components/Profiles/ProfileHeader";
 import ProfileTranslateHistory from "../components/Profiles/ProfileTranslateHistory";
-import React, { useEffect } from "react"
+import React from "react"
 import withAuth from '../hoc/withAuth';
-import { findUserById } from '../api/user';
 import { useUser } from '../context/UserContext';
 
 
 const Profile = () => {
-    const { user, setUser} = useUser()
-    useEffect(() =>{
-        const findUser = async () => {
-            const [error, latestUser] = await findUserById(user.id)
-            if (error === null) {
-                setUser(latestUser)
-            }
-        }
-        // findUser()
-    },[])
+    const { user } = useUser()
 	return (
     <div id="profile">
-			{/* <h2>Profile</h2> */}
 			<ProfileHeader username={user.username} />
 			<ProfileTranslateHistory translations={user.translations} />
       <ProfileAction />
